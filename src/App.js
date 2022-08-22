bad35565865acbbed286cb70d75a81ed7ecac16a
Iouri Volkov <volk.iouri@gmail.com>
2022-08-21 23:40:41 -0400

    Create folder structure, create LandingPage component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import LandingPage from "../src/components/LandingPage";
+
+const landingPage = {
+  title: "Hello. I'm a full-stack developer.",
+  subtitle:
+    "My name is Iouri Volkov. I design intuitive interfaces and produce clean code using modern web technologies.",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LandingPage title={landingPage.title} subtitle={landingPage.subtitle} />
     </div>
   );
 }
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,86 @@
+import { Stack, Typography, Button } from "@mui/material";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+};
+
+const LandingPage = ({ title, subtitle }) => {
+  return (
+    <Stack
+      sx={{
+        ...style,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Typography
+        sx={{
+          textAlign: "center",
+          fontFamily: "Futura",
+          color: "#FF7575",
+          fontSize: "64px",
+        }}
+      >
+        {title}
+      </Typography>
+
+      <Typography
+        sx={{
+          textAlign: "center",
+          fontFamily: "Futura",
+          color: "#FF7575",
+          fontSize: "24px",
+        }}
+      >
+        {subtitle}
+      </Typography>
+      <Button
+        disableRipple
+        disableElevation
+        variant="outlined"
+        sx={{
+          border: "3px solid #FF7575",
+          textTransform: "none",
+          color: "#FF7575",
+          borderRadius: "30px",
+          width: "300px",
+          height: "60px",
+          marginTop: "100px",
+          "&:hover": {
+            bgcolor: "#FF7575",
+            color: "white",
+            cursor: "pointer",
+            border: "none",
+          },
+        }}
+      >
+        Let's collaborate!
+      </Button>
+      <Button
+        disableElevation
+        disableRipple
+        sx={{
+          borderRadius: 100,
+          width: 60,
+          height: 60,
+          marginTop: "75px",
+          bgcolor: "#FF7575",
+          color: "white",
+          "&:hover": {
+            bgcolor: "#FF7576",
+          },
+        }}
+      >
+        <ArrowDownwardIcon />
+      </Button>
+    </Stack>
+  );
+};
+
+export default LandingPage;
